92f696be8c2f48ef00fc5f32bd260a8b6ef4392f
Htet Naing <htetnaing0814@gmail.com>
2022-07-26 19:00:26 +0630

    users can now like the pokemon

--- modules/api.js
+++ modules/api.js
@@ -1,22 +1,32 @@
+import {
+    getLikes
+} from './like.js';
 import pokemons from './pokemons.js';
 
 const pokemonApi = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/';
 
-const load = () => {
-  const container = document.getElementById('container');
-  for (let i = 0; i < pokemons.length; i += 1) {
-    container.innerHTML += `
+const load = async () => {
+    const likes = await getLikes();
+    const container = document.getElementById('container');
+    for (let i = 0; i < pokemons.length; i += 1) {
+        let like = likes.filter(like => like.item_id === pokemons[i].name);
+        if (like.length === 0) {
+            like = 0;
+        } else {
+            like = like[0].likes;
+        }
+        container.innerHTML += `
     <div id="card">
         <img id="image"
             src=${pokemonApi + pokemons[i].image}
             alt=${pokemons[i].name}>
         <div id="title">
             <p>${pokemons[i].name}</p>
-            <div><i id="like${pokemons[i].name}" class="fa-regular fa-heart"></i><br>5 likes</div>
+            <div><i id="like${pokemons[i].name}" class="fa-regular fa-heart"></i><br>${ like } likes</div>
         </div>
         <button>comment</button>
     </div>`;
-  }
+    }
 };
 
 export default load;
--- modules/like.js
+++ modules/like.js
@@ -1,33 +1,37 @@
 import pokemons from './pokemons.js';
 
-// const involvementApi = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/DYVkVDoJjOcdS6u668lb/likes';
+const invApi = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/DYVkVDoJjOcdS6u668lb/likes';
 
-const like = async (name) => {
-  console.log(name);
-  // console.log(name);
-  //     const data = {
-  //     method: 'POST',
-  //     body: JSON.stringify({
-  //       item_id: name,
-  //     }),
-  //     headers: {
-  //       'Content-type': 'application/json; charset=UTF-8',
-  //     },
-  //   };
+const getLikes = async () => {
+    const req = new Request(invApi);
+    const res = await fetch(req);
+    const json = await res.json();
+    return json;
+}
 
-  //   const req = new Request(involvementApi);
-  //   await fetch(req, data);
-  //   window.location.reload();
+const postLike = async (name) => {
+    const data = {
+        method: 'POST',
+        body: JSON.stringify({
+            item_id: name,
+        }),
+        headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+        },
+    };
+
+    const req = new Request(invApi);
+    await fetch(req, data);
+    window.location.reload();
 };
 
-const love = () => {
-  for (let i = 0; i < pokemons.length; i += 1) {
-    const love = document.getElementById(`like${pokemons[i].name}`);
-    love.addEventListener('click', () => {
-      // console.log('name');
-      like(pokemons[i].name);
-    });
-  }
+const likeButton = () => {
+    for (let i = 0; i < pokemons.length; i += 1) {
+        const love = document.getElementById(`like${pokemons[i].name}`);
+        love.addEventListener('click', () => {
+            postLike(pokemons[i].name);
+        });
+    }
 };
 
-export default love;
+export { likeButton, getLikes };
