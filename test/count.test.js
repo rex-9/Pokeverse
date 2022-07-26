30d87e3864356afb3775d116d848c449bd4d028c
Htet Naing <htetnaing0814@gmail.com>
2022-07-26 21:45:19 +0630

    add test for pokemon count on home page

---
+++ test/count.test.js
@@ -0,0 +1,12 @@
+import pokemonCount from '../modules/count.js';
+
+document.body.innerHTML = `
+<ul class="nav-content">
+
+</ul>
+`
+test('Check the pokemon count', () => {
+  pokemonCount();
+  const total = document.getElementById('pokemons');
+  expect(total.innerHTML).toEqual('Pokemons(9)');
+});
