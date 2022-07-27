0dc7b657f88cc5ced6cad524505e1682001ca87d
ibizugbe <abrahamibizugbe@gmail.com>
2022-07-27 05:10:17 +0100

    make remove mobilemenu function

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 import load from "../modules/api.js";
 import love from "../modules/like.js";
-import displayMenu from "../modules/mobileMenu.js";
 import popUpMenu from "../modules/comment.js";
 import "./style.css";
 
-const logo = require("../");
 const desktopNav = document.querySelector(".nav-ul");
 const mobileContainer = document.querySelector(".mobile-menu");
 const cancelBtn = document.querySelector(".cancel");
