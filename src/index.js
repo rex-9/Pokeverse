1007c4c2056b420e54e4a93e1e53e25a1b9c7a11
ibizugbe <abrahamibizugbe@gmail.com>
2022-07-27 11:11:41 +0100

    add brand image, update readme and modal.

--- src/index.js
+++ src/index.js
@@ -1,20 +1,26 @@
-import { load } from '../modules/load.js';
-import { pokemonCount } from '../modules/count.js';
-import { likeButton } from '../modules/like.js';
-import './style.css';
-import popUpMenu from '../modules/comment.js';
+import { load } from "../modules/load.js";
+import { pokemonCount } from "../modules/count.js";
+import { likeButton } from "../modules/like.js";
+import "./style.css";
+import popUpMenu from "../modules/comment.js";
+import pokemon from "../Assets/pokemon.png";
 
-const desktopNav = document.querySelector('.nav-ul');
-const mobileContainer = document.querySelector('.mobile-menu');
-const cancelBtn = document.querySelector('.cancel');
-const hamburger = document.querySelector('.bars');
-hamburger.addEventListener('click', () => {
-  mobileContainer.style.display = 'flex';
-  desktopNav.style.display = 'none';
+const brandContainer = document.querySelector(".nav-brand");
+const desktopNav = document.querySelector(".nav-ul");
+const mobileContainer = document.querySelector(".mobile-menu");
+const cancelBtn = document.querySelector(".cancel");
+const hamburger = document.querySelector(".bars");
+const brandImage = new Image();
+brandImage.src = pokemon;
+brandImage.classList.add("brand-image");
+brandContainer.appendChild(brandImage);
+hamburger.addEventListener("click", () => {
+  mobileContainer.style.display = "flex";
+  desktopNav.style.display = "none";
 });
-cancelBtn.addEventListener('click', () => {
-  mobileContainer.style.display = 'none';
-  desktopNav.style.display = 'flex';
+cancelBtn.addEventListener("click", () => {
+  mobileContainer.style.display = "none";
+  desktopNav.style.display = "flex";
 });
 
 await load();
